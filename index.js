f86d70a869f3d0be13b8d760def6baed94f60d48
Zlata Smirnova <smzlata0496@gmail.com>
2024-04-04 00:48:42 +0300

    make function to change task status and sorting by status

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const init = () => {
+const displayTaskList = () => {
     const todoListHTMLBlock = document.querySelector('.todo__list')
     const todoListStorage = JSON.parse(localStorage.getItem('todo'))
-    
-    if (todoListStorage) {
+
+    if (todoListStorage && todoListStorage.length> 0) {
+        todoListStorage.sort((a) => a.includes('isdone="true"') ? 1 : -1)
         let todoListItems = ''
         for (let item of todoListStorage) {
             todoListItems += item
@@ -11,24 +12,37 @@ const init = () => {
     } else {
         todoListHTMLBlock.innerHTML = `<span class="todo__list__no_elements">Нет никаких дел</span>`
     }
-    const newTaskInput = document.querySelector('#todo__add__input')
-    if (newTaskInput) {
-        newTaskInput.addEventListener('keydown', (e) => {
+    
+    addEvemtListeners()   
+}
+
+const addEvemtListeners = () => {
+    const taskInput = document.querySelector('#todo__add__input')
+    if (taskInput) {
+        taskInput.addEventListener('keydown', (e) => {
             if (e.key === 'Enter') {
-                createTask(newTaskInput)
+                createTask(taskInput)
             }
         })
     }
-    const addTaskButton = document.querySelector('.todo__add__button')
-    if (addTaskButton) {
-        addTaskButton.addEventListener('click', () => createTask(newTaskInput))
+    const addTaskBtn = document.querySelector('.todo__add__button')
+    if (addTaskBtn) {
+        addTaskBtn.addEventListener('click', () => createTask(taskInput))
     }
 
-    const deleteBtn = document.querySelectorAll('.task__delete');
-    deleteBtn.forEach(item => {
+    const deleteTaskBtn = document.querySelectorAll('.task__delete');
+    deleteTaskBtn.forEach(item => {
         item.addEventListener('click', (e) => {
             const task = e.target.closest('.todo__list__item')
             deleteTast(task)
+        } )
+    })
+
+    const status = document.querySelectorAll('.task__status');
+    status.forEach(item => {
+        item.addEventListener('click', (e) => {
+            const task = e.target.closest('.todo__list__item')
+            changeTaskStatus(task)
         } )
     })
 }
@@ -38,24 +52,37 @@ const createTask = (input) => {
         const todoList = JSON.parse(localStorage.getItem('todo')) || []
         const newTask = `<div class="todo__list__item" id=${Date.now()}>
                             <span class="task__name">${input.value}</span>
-                            <select class="task__status" size="1">
-                                <option value="active">Не сделано</option>
-                                <option value="done">Сделано</option>
-                            </select>
+                            <span class="task__status" isdone = false>Не выполнено</span>
                             <button class="task__delete">Удалить</button>
                         </div>`
         todoList.push(newTask)
         localStorage.setItem('todo', JSON.stringify(todoList))
         input.value = ''
-        init()
+        displayTaskList()
     }
 }
 
 const deleteTast = (deletingTask) => {
     let todoList = JSON.parse(localStorage.getItem('todo'))
-    todoList = todoList.filter((task) => !task.includes(`id=${deletingTask.id}`))
+    todoList = todoList.filter((task) => !task.includes(deletingTask.id))
     localStorage.setItem('todo', JSON.stringify(todoList))
-    init()
+    displayTaskList()
 }
 
-init()
+const changeTaskStatus = (changingTask) => {
+    let todoList = JSON.parse(localStorage.getItem('todo'))
+    let taskIndex = todoList.findIndex((task) => task.includes(changingTask.id))
+    let status = changingTask.querySelector('.task__status')
+    if (status.getAttribute('isdone') == 'false') {
+        status.innerHTML = 'Выполнено'
+        status.setAttribute('isdone', 'true')
+    } else {
+        status.innerHTML = 'Не выполнено'
+        status.setAttribute('isdone', 'false')
+    }
+    todoList[taskIndex] = changingTask.outerHTML 
+    localStorage.setItem('todo', JSON.stringify(todoList))
+    displayTaskList()
+}
+
+displayTaskList()
